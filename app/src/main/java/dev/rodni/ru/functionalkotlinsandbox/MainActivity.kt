82ba215f264059c4ed74431811f77be22f8bf1bf
Rodion.Prosvirnin <rodion.prosvirnin@bk.ru>
2020-05-27 17:54:35 +0300

    high-order fun example.

--- app/src/main/java/dev/rodni/ru/functionalkotlinsandbox/MainActivity.kt
+++ app/src/main/java/dev/rodni/ru/functionalkotlinsandbox/MainActivity.kt
@@ -10,13 +10,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        anonymousFunsWithVars()
+
         /**
-         * anonymous functions
+         * example of using high-order function
          */
-        val summary: (Int, Int) -> Int = { firstNum, secondNum -> firstNum + secondNum}
+        doFun { perimeter() }
+    }
+
+    /**
+     * example of the high-order function
+     */
+    private fun doFun(func: () -> Unit) {
+        func()
+    }
+
+    /**
+     * calculates a perimeter of some shape
+     */
+    private fun perimeter() {
+        val perimeter = { firstSide: Int, secondSide: Int -> firstSide*2 + secondSide*2}
+        Log.d("perimeter", "perimeter is: ${perimeter(3, 4)}")
+    }
+
+    private fun anonymousFunsWithVars() {
+        /**
+         * anonymous functions with params
+         */
+        val summary: (Int, Int) -> Int = { firstNum, secondNum -> firstNum + secondNum }
+
+        /**
+         * anonymous functions with no explicit param for the var but with it for the fun
+         */
+        val summaryWithExplicit = { firstNum: Int, secondNum: Int -> firstNum + secondNum }
+
+        /**
+         * anonymous functions without params
+         * this impossible when i have more than one params
+         */
+        val square: (Int) -> Int = { it * it }
+
         //this will print only the exact function which was passed through
         Log.d("summary","function is: $summary")
         //this will print a result of a summary fun
         Log.d("summary","summary is: ${summary(5,7)}")
+
+        //this will print a result of a square fun
+        Log.d("square","square is: ${square(5)}")
     }
 }
